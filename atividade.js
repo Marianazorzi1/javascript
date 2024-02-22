9171d1a21068b8b11e3c12deac3fe09b9f12294d
Marianazorzi1 <m.zorzi@estudante.ifro.edu.br>
2024-02-22 11:06:40 -0400

    Hello

---
+++ atividade.js
@@ -0,0 +1,10 @@
+// var nome = 'Mariana'
+// var idade = 15
+// console.log('Olá, meu nome é ',nome,' e eu tenho',idade,'anos')
+
+// var string = 'Mari liNda'.toUpperCase() 
+// console.log(string)
+
+var num = 25 
+
+console.log(typeof num)
