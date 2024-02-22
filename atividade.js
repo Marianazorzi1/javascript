d5ec2afc2395fa851cc7acfc0c03ff492d0df5a5
Marianazorzi1 <m.zorzi@estudante.ifro.edu.br>
2024-02-22 11:36:57 -0400

    atividades

--- atividade.js
+++ atividade.js
@@ -2,9 +2,41 @@
 // var idade = 15
 // console.log('Olá, meu nome é ',nome,' e eu tenho',idade,'anos')
 
-// var string = 'Mari liNda'.toUpperCase() 
-// console.log(string)
+// var string = 'Mari liNda' 
+// console.log(string .toUpperCase())
 
-var num = 25 
+// var num = 25 
+// console.log(typeof num);
 
-console.log(typeof num);
+// var peso = 52
+// var altura = 1.54
+// console.log(peso/altura*2)
+
+// var soma = 2+5;
+// var subtração = 2-1;
+// var multiplicação = 2*2;
+// var divisão = 4/2;
+// console.log(soma, subtração, multiplicação, divisão);
+
+// var idade1 = 12
+// var idade2 = 14
+// if (idade1> idade2) {console.log('Mais velho')}
+// (idade1 > idade2)
+//     ? console.log(SDKS)
+//     : console.log('WE')
+
+// var idade = 16
+// if (idade >=18) {
+//     console.log('Maior que 18!')
+// }
+// else {
+//     console.log('Menor que 18!')
+// }
+
+// var num = 3
+// if (num%2==0) {
+//     console.log('par')
+// }
+// else {
+//     console.log('impar')
+// }
