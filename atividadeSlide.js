76275e025c232a2ed5b4cb32d983fb134a2049ba
Marianazorzi1 <m.zorzi@estudante.ifro.edu.br>
2024-02-29 11:41:10 -0400

    atividade aula 29

---
+++ atividadeSlide.js
@@ -0,0 +1,53 @@
+// const apartamento = {
+//     quartos : 2,
+//     tipo : 'apartamento',
+//     endereço : 'Avenida principal,465 - Centro',
+//     andar : 7
+// };
+// console.log(apartamento.tipo, "com " + apartamento.quartos, "quartos, localizado " + apartamento.andar, "andar da " + apartamento.endereço )
+
+
+// const produtoEmbalado = {
+//     nome : 'Laptop HP',
+//     categoria : 'Eletrônicos',
+//     peso : 1.5,
+//     preço : 3.500
+// };
+
+// console.log('O produto ' + produtoEmbalado.nome+ ', da categoria ' + produtoEmbalado.categoria + ' , pesando' + produtoEmbalado.peso +'kg, está á venda por R$' + produtoEmbalado.preço )
+
+
+// Crie uma classe chamada Imovel com os seguintes atributos:
+//  quartos:
+//  tipo:
+//  endereco:
+// Crie um método na classe chamado exibirInformacoes que
+// retorna uma string com as informações do imóvel.
+// Em seguida, instancie dois objetos da classe Imovel
+// representando uma casa e um apartamento com as seguintes
+// características:
+// Casa:
+//  Quartos: 4
+//  Tipo: "Casa"
+//  Endereço: "Rua da Amizade, 789 - Bairro Alegre"
+// Apartamento:
+//  Quartos: 2
+//  Tipo: "Apartamento"
+//  Endereço: "Avenida da Paz, 123 - Centro"
+// Chame o método exibirInformacoes para cada instância e exiba o
+// resultado no console.
+
+class Imovél {
+    constructor(quartos){
+        this.quartos = quartos
+    }
+    constructor(tipo){
+        this.tipo = tipo
+    }
+
+    constructor (endereço){
+        this.endereço = endereço
+    }
+    
+
+}
