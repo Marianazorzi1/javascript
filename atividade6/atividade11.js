e43907efcadb8e9513447ef987e8d9157e4dbe0b
Marianazorzi1 <m.zorzi@estudante.ifro.edu.br>
2024-03-28 11:44:09 -0400

    dia28

---
+++ atividade6/atividade11.js
@@ -0,0 +1,29 @@
+// function letras (string){
+//     return string. toUpperCase();
+
+// }
+// var b = letras('a,b,c');
+// console.log(b);
+
+
+
+// function teste (num){
+//     return num%2==0 ? 'Par' : 'Impar';
+    
+// }
+
+// console.log(teste(24));
+
+
+// function mari(num1,num2){
+//     return num1 + num2;
+// }
+// console.log(mari(12,12));
+
+
+// var vlr = function(vlr,porcent){
+//     return vlr = (vlr*porcent/100)
+// }
+// console.log(vlr(150,10));
+
+
