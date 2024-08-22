3032415d4278d33ca513c9fa9c87a6d353fd7fe5
Marianazorzi1 <m.zorzi@estudante.ifro.edu.br>
2024-08-22 11:28:16 -0400

    .

--- API/index.js
+++ API/index.js
@@ -2,10 +2,11 @@ const express = require ('express');
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.json({mensagem: 'Hello World!'})
+    res.json(mensagem)
 });
 
 app.listen(8080, () => {
-    let data = new Date();
-    console.log("Servidor node iniciado em: " + data)
+    let mensagem = ({mensagem: 'Hello World!'})
+    console.log("Servidor node iniciado em: " )
 })
